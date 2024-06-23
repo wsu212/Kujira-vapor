5dce9f168f2bdf76dca62415059c21c42b86043f
wsu212 <wsu212@gmail.com>
2024-06-23 15:46:26 -0400

    create KujiraController

---
+++ Sources/App/Controllers/KujiraController.swift
@@ -0,0 +1,31 @@
+//
+//  KujiraController.swift
+//
+//
+//  Created by Wei-lun Su on 6/23/24.
+//
+
+import Foundation
+import Vapor
+
+struct KujiraController: RouteCollection {
+    func boot(routes: any RoutesBuilder) throws {
+        let kujira = routes.grouped("kujira")
+        kujira.get(use: index)
+        
+        kujira.get(":id", use: show)
+    }
+    
+    @Sendable
+    func index(req: Request) async throws -> String {
+        return "Index"
+    }
+    
+    @Sendable
+    func show(req: Request) async throws -> String {
+        guard let id = req.parameters.get("id") else {
+            throw Abort(.internalServerError)
+        }
+        return "The ID: \(id)"
+    }
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,49 +1,5 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    
-    let kujira = app.grouped("kujira")
-    let users = app.grouped("users")
-    
-    // MARK: - GET
-    
-    // ~/kujira/get/string_variable
-    kujira.get("get", "string", ":countryCode") { request async throws in
-        guard let countryCode = request.parameters.get("countryCode", as: String.self) else {
-            throw Abort(.badRequest)
-        }
-        return "kujira: \(countryCode)\n\(request.parameters)"
-    }
-    
-    // ~/kujira/get/integer_variable
-    kujira.get("get", "integer", ":id") { request async throws in
-        guard let id = request.parameters.get("id", as: Int.self) else {
-            throw Abort(.badRequest)
-        }
-        return "kujira: \(id)"
-    }
-    
-    // ~/kujira/get/models
-    kujira.get("get", "models") { request async in
-        return Model.mocks
-    }
-    
-    // ~/kujira/get/model/query?sort=asc&search=taiwan
-    kujira.get("get", "models", "query") { request async throws in
-        let query = try request.query.decode(Query.self)
-        return query
-    }
-    
-    // ~/users/get/premium
-    users.get("premium") { request async in
-        return "Premium"
-    }
-    
-    // MARK: - POST
-    
-    // ~/kujira/post/model
-    kujira.post("post", "model") { request async throws in
-        let model = try request.content.decode(Model.self)
-        return model
-    }
+    try app.register(collection: KujiraController())
 }
