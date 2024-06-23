0d492761ba668750b227ff76b64b71cfcda44b9d
wsu212 <wsu212@gmail.com>
2024-06-23 15:28:44 -0400

    group routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,14 +1,14 @@
 import Vapor
 
 func routes(_ app: Application) throws {
+    
+    let kujira = app.grouped("kujira")
+    let users = app.grouped("users")
+    
     // MARK: - GET
     
-    app.get { request async in
-        "\(request.parameters)"
-    }
-    
     // ~/kujira/get/string_variable
-    app.get("kujira", "get", "string", ":countryCode") { request async throws in
+    kujira.get("get", "string", ":countryCode") { request async throws in
         guard let countryCode = request.parameters.get("countryCode", as: String.self) else {
             throw Abort(.badRequest)
         }
@@ -16,7 +16,7 @@ func routes(_ app: Application) throws {
     }
     
     // ~/kujira/get/integer_variable
-    app.get("kujira", "get", "integer", ":id") { request async throws in
+    kujira.get("get", "integer", ":id") { request async throws in
         guard let id = request.parameters.get("id", as: Int.self) else {
             throw Abort(.badRequest)
         }
@@ -24,20 +24,25 @@ func routes(_ app: Application) throws {
     }
     
     // ~/kujira/get/models
-    app.get("kujira", "get", "models") { request async in
+    kujira.get("get", "models") { request async in
         return Model.mocks
     }
     
     // ~/kujira/get/model/query?sort=asc&search=taiwan
-    app.get("kujira", "get", "models", "query") { request async throws in
+    kujira.get("get", "models", "query") { request async throws in
         let query = try request.query.decode(Query.self)
         return query
+    }
+    
+    // ~/users/get/premium
+    users.get("premium") { request async in
+        return "Premium"
     }
     
     // MARK: - POST
     
     // ~/kujira/post/model
-    app.post("kujira", "post", "model") { request async throws in
+    kujira.post("post", "model") { request async throws in
         let model = try request.content.decode(Model.self)
         return model
     }
