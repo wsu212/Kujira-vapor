f57b8e4bbb0328ea066bbd7483198620a4647b14
wsu212 <wsu212@gmail.com>
2024-06-23 14:33:25 -0400

    handle POST requests

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,29 +1,38 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    // http://127.0.0.1:8080
+    // MARK: - GET
+    
     app.get { request async in
         "\(request.parameters)"
     }
     
-    // http://127.0.0.1:8080/kujira/string_variable
-    app.get("kujira", "string", ":countryCode") { request async throws in
+    // ~/kujira/get/string_variable
+    app.get("kujira", "get", "string", ":countryCode") { request async throws in
         guard let countryCode = request.parameters.get("countryCode", as: String.self) else {
             throw Abort(.badRequest)
         }
         return "kujira: \(countryCode)\n\(request.parameters)"
     }
     
-    // http://127.0.0.1:8080/kujira/integer_variable
-    app.get("kujira", "integer", ":id") { request async throws in
+    // ~/kujira/get/integer_variable
+    app.get("kujira", "get", "integer", ":id") { request async throws in
         guard let id = request.parameters.get("id", as: Int.self) else {
             throw Abort(.badRequest)
         }
         return "kujira: \(id)"
     }
     
-    // http://127.0.0.1:8080/kujira/models
-    app.get("kujira", "models") { request async in
+    // ~/kujira/get/models
+    app.get("kujira", "get", "models") { request async in
         return Model.mocks
+    }
+    
+    // MARK: - POST
+    
+    // ~/kujira/post/model
+    app.post("kujira", "post", "model") { request async throws in
+        let model = try request.content.decode(Model.self)
+        return model
     }
 }
