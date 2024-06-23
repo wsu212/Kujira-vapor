926d7f3ecf8bc2bc511045be2f5f1ac7f0eb5480
wsu212 <wsu212@gmail.com>
2024-06-22 21:27:37 -0400

    add basic routes and route parameters

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,30 @@
 import Vapor
 
 func routes(_ app: Application) throws {
+    // http://127.0.0.1:8080
     app.get { req async in
         "It works!"
     }
 
+    // http://127.0.0.1:8080/hello
     app.get("hello") { req async -> String in
         "Hello, world!"
+    }
+    
+    // http://127.0.0.1:8080/movies/dynamic_parameter
+    app.get("movies", ":genre") { req async throws -> String in
+        guard let genre = req.parameters.get("genre") else {
+            throw Abort(.badRequest)
+        }
+        return "All \(genre) movies."
+    }
+    
+    // http://127.0.0.1:8080/movies/dynamic_parameter/dynamic_parameter
+    app.get("movies", ":genre", ":year") { req async throws -> String in
+        guard let genre = req.parameters.get("genre"),
+              let year = req.parameters.get("year") else {
+            throw Abort(.badRequest)
+        }
+        return "All \(year) \(genre) movies."
     }
 }
