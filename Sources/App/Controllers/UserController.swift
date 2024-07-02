d983496470b5591e38da6581625cc5c623506a4c
wsu212 <wsu212@gmail.com>
2024-07-02 08:58:45 -0400

    implement Login route

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -15,6 +15,34 @@ final class UserController: RouteCollection, Sendable {
         api.post("register", use: register)
     }
     
+    func login(req: Request) async throws -> String {
+        // decode the request
+        let user = try req.content.decode(User.self)
+        
+        // check if the user exists in the database
+        guard let existingUser = try await User.query(on: req.db)
+            .filter(\.$username == user.username)
+            .first() else {
+                throw Abort(.badRequest)
+            }
+        
+        // validate the password
+        let isVerified = try await req.password
+            .async
+            .verify(
+                user.password,
+                created: existingUser.password
+            )
+        
+        if !isVerified {
+            throw Abort(.unauthorized)
+        }
+        
+        // generate the token and return it to the user
+        
+        return ""
+    }
+    
     @Sendable
     func register(req: Request) async throws -> RegisterResponseDTO {
         // validate the user
