247023f49dc2729e7a59745be1cde84693920d76
Wei-Lun Su <48189264+wsu212@users.noreply.github.com>
2024-07-09 20:48:59 -0400

    implement Item Model

---
+++ Sources/App/Models/Item.swift
@@ -0,0 +1,40 @@
+//
+//  Item.swift
+//  
+//
+//  Created by Wei-lun Su on 7/9/24.
+//
+
+import Foundation
+import Vapor
+import Fluent
+
+final class Item: Model {
+    static let schema = "items"
+    
+    @ID(key: .id)
+    var id: UUID?
+    
+    @Field(key: "title")
+    var title: String
+    
+    @Field(key: "price")
+    var price: Double
+    
+    @Field(key: "quantity")
+    var quantity: Int
+    
+    // foreign key
+    @Parent(key: "categoryId")
+    var category: Category
+    
+    init() { }
+    
+    init(id: UUID? = nil, title: String, price: Double, quantity: Int, categoryId: UUID) {
+        self.id = id
+        self.title = title
+        self.price = price
+        self.quantity = quantity
+        self.$category.id = categoryId
+    }
+}
